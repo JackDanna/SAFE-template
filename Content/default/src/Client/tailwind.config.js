b35f34988c4acdba21cf6ce484e3889130dd76b1
JackDanna <jackdanna78@gmail.com>
2025-09-18 10:32:57 -0400

    Code clean up

--- Content/default/src/Client/tailwind.config.js
+++ Content/default/src/Client/tailwind.config.js
@@ -9,5 +9,15 @@ module.exports = {
     theme: {
         extend: {},
     },
-    plugins: []
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula"],
+        darkTheme: "dark",
+        base: true,
+        styled: true,
+        utils: true,
+        prefix: "",
+        logs: true,
+        themeRoot: ":root",
+    },
 }
